7e9d6e594783399c0369dcc6cd0759885bfda5c9
Aditya kumar <134756409+rajput204@users.noreply.github.com>
2025-06-16 17:00:01 +0530

    again

--- StriverSheet/Arrays/Logic_building/Sprint1/prog5.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-void sort(int a[],int size)
-{
-    
-}
--- StriverSheet/Arrays/STL/Vectors/lambda1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=4,b=5;
-    [=]()->void{cout<<a<<" "<<b;}();//refernce by copy ,auto can be used in place of void
-    return 0;
-}
-
